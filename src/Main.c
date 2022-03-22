57e13e5277814c8bbf1c63fd3df87acb309e1bc8
Bustamante Mathias <48842768+BustamanteMathias@users.noreply.github.com>
2022-03-22 13:45:51 -0300

    Collections

---
+++ src/Main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Examples.h"
+
+
+int main(void)
+{
+	//example1(); // Dict
+	//example2(); // Set
+	//example3(); // LinkedList
+	example4(); // Collections
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
